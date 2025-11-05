25fc0a0f8347981070a81de4b3ad8e9d618dfdb8
VIVEK CHOPADE <chopadevivek4466@gmail.com>
2025-11-05 15:04:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins & PM2, this is my webhook chnage');
+  res.send('Hello from Jenkins & PM2, this is my webhook&deploy on secont EC2');
 });
 
 app.listen(port, () => {
